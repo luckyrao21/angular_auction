e3861120fc78dbc253376a9d65120260a78ecf72
Narendra Pipliwal <rajkasotiya26@gmail.com>
2022-04-23 23:18:07 +0530

    Raj

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -15,17 +15,21 @@ export class AddProductComponent implements OnInit {
   startDate: string = '';
   endDate: string = '';
   image: any = '';
-  productImage:any;
-  categoryList:any[] = [];
+  productImage: any;
+  categoryList: any[] = [];
+  startHours: any;
+  endHours: any;
+
+
   constructor(private product: ProductService) {
-    this.product.viewCategory().subscribe(data=>{
-       if(!data.error){
-         this.categoryList = data;
-       }
+    this.product.viewCategory().subscribe(data => {
+      if (!data.error) {
+        this.categoryList = data;
+      }
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   public onSelect(e: any) {
     if (e.target.files) {
@@ -40,24 +44,25 @@ export class AddProductComponent implements OnInit {
   }
 
   public addNewProduct() {
+    console.log(this.startDate + " " + this.startHours + ":00:00");
     const formData = new FormData();
-    formData.append('productName',this.productName);
-    formData.append('productDesc',this.productDesc);
+    formData.append('productName', this.productName);
+    formData.append('productDesc', this.productDesc);
     formData.append('productInitialPrice', this.productInitialPrice);
-    formData.append('creator',"624ed1ba90660765236fe26a");
-    formData.append('startTime',this.startDate);
-    formData.append('endTime',this.endDate);
-    formData.append('categoryName',this.productCategory);
-    formData.append('productImage',this.productImage);
+    formData.append('creator', "624ed1ba90660765236fe26a");
+    formData.append('startTime', this.startDate + " " + this.startHours + ":00:00");
+    formData.append('endTime', this.endDate + " " + this.endHours + ":00:00");
+    formData.append('categoryName', this.productCategory);
+    formData.append('productImage', this.productImage);
 
     this.product.addProduct(formData).subscribe((data) => {
-       if(data.data){
-            // this.ngOnInit()
-            alert('Product Added Successfully..');
-            this.ngOnInit()
-       }
-       else
-            alert('Product Not Added');
+      if (data.data) {
+        // this.ngOnInit()
+        alert('Product Added Successfully..');
+        this.ngOnInit()
+      }
+      else
+        alert('Product Not Added');
 
     });
   }
